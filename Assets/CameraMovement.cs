f1b0c95c37bdee6faaa3725b08e6912de2a8a83e
Prokop Randáček <prokop.randacek@gmail.com>
2020-01-14 19:51:42 +0100

    Camera movement

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform PlayerPos;
+    public float minX;
+    public float maxX;
+    private Vector3 pos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPos.transform.position.x > minX && PlayerPos.transform.position.x < maxX)
+        {
+            pos = new Vector3(PlayerPos.transform.position.x, transform.position.y, transform.position.z);
+            transform.position = pos;
+        }
+    }
+}
