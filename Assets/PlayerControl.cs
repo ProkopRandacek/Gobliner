f113c666ff37070a161a8d6351e43391fc9ca42d
Prokop Randáček <prokop.randacek@gmail.com>
2020-01-14 17:18:55 +0100

    Player Movement

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float speed;
+    private bool Jump;
+    private float Horizontal = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxis("Horizontal") * speed;
+        if (Input.GetButtonDown("Jump"))
+            Jump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(Horizontal * Time.fixedDeltaTime, Jump);
+        Jump = false;
+    }
+}
