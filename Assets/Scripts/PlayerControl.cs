317945103f89a249fc9f4e2c84a46c16cdd4e070
Prokop Randáček <prokop.randacek@gmail.com>
2020-02-01 14:42:57 +0100

    Player animation

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,21 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public CharacterController2D controller;
     public float speed;
+
+    private CharacterController2D controller;
+    private Animator animator;
+    private Rigidbody2D rb;
     private bool Jump;
     private float Horizontal = 0f;
 
-
-    // Update is called once per frame
+    private void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
         Horizontal = Input.GetAxis("Horizontal") * speed;
+
         if (Input.GetButtonDown("Jump"))
+        {
             Jump = true;
+            animator.SetTrigger("jump");
+        }
+
+        animator.SetFloat("fall", rb.velocity.y);
+        animator.SetFloat("speed", Mathf.Abs(Horizontal));
+    }
+    public void OnLanding()
+    {
+        animator.SetBool("jump", false);
     }
 
     private void FixedUpdate()
