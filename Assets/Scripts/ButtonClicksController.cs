478a1c7dd79cd38d878f9735f6db829debfa0087
Prokop Randáček <prokop.randacek@gmail.com>
2020-02-16 13:36:43 +0100

    Added scenes

---
+++ Assets/Scripts/ButtonClicksController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClicksController : MonoBehaviour
+{
+    public void OnPlayClick()
+    {
+        Debug.Log("Play");
+    }
+
+    public void OnExitClick()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
