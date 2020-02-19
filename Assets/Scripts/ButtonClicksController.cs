5780ec2da2b7854c350c067229041ea5f39c20b5
Prokop Randáček <prokop.randacek@gmail.com>
2020-02-19 22:10:08 +0100

    Added menu

--- Assets/Scripts/ButtonClicksController.cs
+++ Assets/Scripts/ButtonClicksController.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonClicksController : MonoBehaviour
 {
-    public void OnPlayClick()
-    {
-        Debug.Log("Play");
-    }
-
     public void OnExitClick()
     {
-        Debug.Log("Exit");
         Application.Quit();
     }
+
+    public void OnNewGameClick()
+    {
+        On1Click();
+    }
+
+    public void OnContinueClick()
+    {
+
+    }
+
+    public void OnBackToMenuClick() { SceneManager.LoadScene("Scenes/Menu"); }
+
+    public void OnPlayClick()   { SceneManager.LoadScene("Scenes/GameSelect"); }
+    public void OnLevelsClick() { SceneManager.LoadScene("Scenes/Levels");     }
+
+    public void On1Click()  { SceneManager.LoadScene("Scenes/Levels/1");  }
+    public void On2Click()  { SceneManager.LoadScene("Scenes/Levels/2");  }
+    public void On3Click()  { SceneManager.LoadScene("Scenes/Levels/3");  }
+    public void On4Click()  { SceneManager.LoadScene("Scenes/Levels/4");  }
+    public void On5Click()  { SceneManager.LoadScene("Scenes/Levels/5");  }
+    public void On6Click()  { SceneManager.LoadScene("Scenes/Levels/6");  }
+    public void On7Click()  { SceneManager.LoadScene("Scenes/Levels/7");  }
+    public void On8Click()  { SceneManager.LoadScene("Scenes/Levels/8");  }
+    public void On9Click()  { SceneManager.LoadScene("Scenes/Levels/9");  }
+    public void On10Click() { SceneManager.LoadScene("Scenes/Levels/10"); }
 }
---
+++ Assets/Scripts/Unlocker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Unlocker : MonoBehaviour
+{
+    public int Me;
+    public Image image;
+    public Sprite[] sprites;
+
+    private int Level;
+
+    void Start()
+    {
+        Level = PlayerPrefs.GetInt("Score", 1);
+
+        Debug.Log(Level);
+
+        if (Level > Me)
+        {
+            Debug.Log(2);
+            image.sprite = sprites[2];
+        }
+        else if (Level < Me)
+        {
+            Debug.Log(1);
+            image.sprite = sprites[1];
+        }
+        else
+        {
+            Debug.Log(0);
+            image.sprite = sprites[0];
+        }
+    }
+}
