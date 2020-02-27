4094435d7762b68e2470d196ceefbedcafaf7082
Prokop Randáček <prokop.randacek@gmail.com>
2020-02-27 12:03:16 +0100

    Added esc menu

---
+++ Assets/Scripts/EscMenuController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscMenuController : MonoBehaviour
+{
+    public GameObject Menu;
+    void Start()
+    {
+        Menu.GetComponent<Canvas>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Menu.GetComponent<Canvas>().enabled = true;
+        }
+    }
+    public void Hide()
+    {
+        Menu.GetComponent<Canvas>().enabled = false;
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Scenes/Levels");
+    }
+}
