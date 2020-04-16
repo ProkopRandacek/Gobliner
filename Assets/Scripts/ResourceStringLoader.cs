462b4e32c828109e6324519ce0e7544d04c4bcbe
Prokop Randáček <prokop@randacek.dev>
2020-04-16 15:22:37 +0200

    Resource string implementation

---
+++ Assets/Scripts/ResourceStringLoader.cs
@@ -0,0 +1,35 @@
+﻿using TinyJson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceStringLoader : MonoBehaviour
+{
+    string ResourceStringPath = "/ResourceString.json";
+
+    void Start()
+    {
+        Debug.Log(LoadString());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    string LoadString()
+    {
+        string filePath = "/" + path.Replace(".json", "");
+ 
+        TextAsset targetFile = Resources.Load<TextAsset>(filePath);
+
+	return targetFile.text;
+    }
+
+    Dictionary Json2Dict(string json)
+    {
+        string fileJson = File.ReadAllText("test.json");
+        List<int> fileValues = fileJson.FromJson<List<int>>();
+    }
+}
+
