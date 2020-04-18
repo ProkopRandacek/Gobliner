2321e7a49016085c9a18bf27b32a956f4c6226e4
Prokop Randáček <prokop@randacek.dev>
2020-04-18 12:36:12 +0200

    Resource string done

--- Assets/Scripts/ResourceStringLoader.cs
+++ Assets/Scripts/ResourceStringLoader.cs
@@ -1,35 +1,30 @@
-﻿using TinyJson;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourceStringLoader : MonoBehaviour
 {
-    string ResourceStringPath = "/ResourceString.json";
+    public string name;
+	private Text t;
 
     void Start()
     {
-        Debug.Log(LoadString());
-    }
-
-    void Update()
-    {
-        
+		t = transform.Find("Text").gameObject.GetComponent<Text>();
+		t.text = LoadString();
     }
 
     string LoadString()
     {
-        string filePath = "/" + path.Replace(".json", "");
- 
-        TextAsset targetFile = Resources.Load<TextAsset>(filePath);
-
-	return targetFile.text;
-    }
-
-    Dictionary Json2Dict(string json)
-    {
-        string fileJson = File.ReadAllText("test.json");
-        List<int> fileValues = fileJson.FromJson<List<int>>();
+		TextAsset ta = Resources.Load("ResourceString") as TextAsset;
+		string[] text = ta.text.Split('\n');
+		foreach (string line in text)
+		{
+			if (line.Split(';')[0].ToLower() == name.ToLower())
+			{
+				return line.Split(';')[1];
+			}
+		}
+		return "";
     }
 }
-
