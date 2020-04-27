c8f5ac020b7d12e572e8f536e6b4c4642d2727c5
Prokop Randáček <prokop@randacek.dev>
2020-04-27 16:17:01 +0200

    Added intro

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+	public Image img;
+
+	private float time = 0;
+
+	void Update()
+	{
+
+		time += Time.deltaTime;
+
+		if (time < 1)
+		{
+			img.color = new Color(1, 1, 1, time - 0);
+		}
+		else if (time < 3)
+		{
+			
+		}
+		else if (time <= 4)
+		{
+			img.color = new Color(1, 1, 1, 1 - (time - 3));
+		}
+		else if (time > 4.5)
+		{
+			SceneManager.LoadScene("Scenes/Menu");
+		}
+	}
+}
